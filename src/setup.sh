47d65e9fb3063f4318a060da3ce90ad9e72e4dd9
Kamran <you@example.com>
2019-11-07 09:00:51 -0800

    Replace protobuf 3.5.1 with 3.9.2

--- src/setup.sh
+++ src/setup.sh
@@ -8,14 +8,14 @@ mkdir "$TEMP_FOLDER"
 # Build and install protobuf
 mkdir "$TEMP_FOLDER/protobuf-build"
 
-( cd "$TEMP_FOLDER/protobuf-build" && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -Dprotobuf_BUILD_TESTS:BOOL=false -Dprotobuf_BUILD_SHARED_LIBS:BOOL=true "$PACKAGE_FOLDER/protobuf-master/cmake")
+( cd "$TEMP_FOLDER/protobuf-build" && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -Dprotobuf_BUILD_TESTS:BOOL=false -Dprotobuf_BUILD_SHARED_LIBS:BOOL=true "$PACKAGE_FOLDER/protobuf/cmake")
 
 ( cd "$TEMP_FOLDER/protobuf-build" && make -j8 && sudo make -j8 install && sudo ldconfig)
 
 # Build and install VTK
 mkdir "$TEMP_FOLDER/vtk-build"
 
-( cd "$TEMP_FOLDER/vtk-build" && cmake -DCMAKE_BUILD_TYPE:STRING=Release -DVTK_Group_Qt:BOOL=true "$PACKAGE_FOLDER/vtk/8.1.0/src")
+( cd "$TEMP_FOLDER/vtk-build" && cmake -DCMAKE_BUILD_TYPE:STRING=Release -DVTK_Group_Qt:BOOL=false "$PACKAGE_FOLDER/vtk/8.1.0/src")
 
 ( cd "$TEMP_FOLDER/vtk-build" && sudo make -j8 install)
 
