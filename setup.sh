7360b703c25acad7e0be67b983683bda38b902d1
Kamran <you@example.com>
2019-11-07 10:08:28 -0800

    built and tested on Ubuntu 16.04 with pb 3.9.2

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export PACKAGE_FOLDER=$(pwd)
+
+# Create a temporary installation directory
+export TEMP_FOLDER=/tmp/install_eit
+mkdir "$TEMP_FOLDER"
+
+# Build and install protobuf
+mkdir "$TEMP_FOLDER/protobuf-build"
+
+( cd "$TEMP_FOLDER/protobuf-build" && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -Dprotobuf_BUILD_TESTS:BOOL=false -Dprotobuf_BUILD_SHARED_LIBS:BOOL=true "$PACKAGE_FOLDER/protobuf/cmake")
+
+( cd "$TEMP_FOLDER/protobuf-build" && make -j8 && sudo make -j8 install && sudo ldconfig)
+
+# Build and install VTK
+mkdir "$TEMP_FOLDER/vtk-build"
+
+( cd "$TEMP_FOLDER/vtk-build" && cmake -DCMAKE_BUILD_TYPE:STRING=Release -DVTK_Group_Qt:BOOL=false "$PACKAGE_FOLDER/vtk/8.1.0/src")
+
+( cd "$TEMP_FOLDER/vtk-build" && sudo make -j8 install)
+
+rm -r "$TEMP_FOLDER"
+
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tarsim/lib' >> ~/.bashrc
+
+source ~/.bashrc
--- thirdparty/vtk/8.1.0/src/CMake/vtkWkHtmlToImage.cmake
+++
@@ -1,3 +0,0 @@
-# Find the wkhtmltoimage executable for testing SVG outputs:
-find_program(VTK_WKHTMLTOIMAGE_EXECUTABLE wkhtmltoimage)
-mark_as_advanced(VTK_WKHTMLTOIMAGE_EXECUTABLE)
